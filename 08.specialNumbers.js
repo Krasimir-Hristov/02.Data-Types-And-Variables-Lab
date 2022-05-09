d45dfa9c75de85203fb3782956024c0ca17ed164
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-05-09 17:29:38 +0200

    Add files via upload

---
+++ 08.specialNumbers.js
@@ -0,0 +1,43 @@
+function solve(number){
+
+    let num = Number(number);
+    
+
+    // for(let i = 1; i <= num; i ++){
+    //     let sum = 0;
+    //     let tempNum = i;
+    //     while(tempNum > 0){
+    //         sum += tempNum % 10;
+    //         tempNum = Math.floor(tempNum / 10);
+    //     }
+    //     let isSpecial
+    //     if(sum === 5 || sum === 7 || sum === 11){
+    //         isSpecial = "True"
+    //     }else{
+    //         isSpecial = "False"
+    //     }
+
+
+    //     console.log(`${i} -> ${isSpecial}`)
+    // }
+    /////////////////////////////////////////////////
+    for(let i = 1; i <= num; i ++){
+        let sum = 0;
+        let stringNum = i.toString();
+        for(let j = 0; j < stringNum.length; j++){
+            sum += Number(stringNum[j]);
+        }
+        let isSpecial
+        if(sum === 5 || sum === 7 || sum === 11){
+             isSpecial = "True"
+        }else{
+            isSpecial = "False"
+       }
+
+
+        console.log(`${i} -> ${isSpecial}`)
+    }
+    
+
+}
+solve(15)
